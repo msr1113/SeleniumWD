41852524e93399806d8b03089cc979006ed9056c
mrawal <imsr1013@gmail.com>
2022-07-08 09:48:04 +0545

    open firefox browser using gecko driver

--- basicweb/firefox_driver_linux.py
+++ basicweb/firefox_driver_linux.py
@@ -1,3 +1,11 @@
 from selenium import webdriver
 
-driver = webdriver.Firefox()
+class RunFFTests():
+    def testMethod(self):
+        # instantiate ff browser command
+        driver = webdriver.Firefox(executable_path="/home/mrawal/Downloads/geckodriver")
+        # open provided url
+        driver.get("https://www.letskodeit.com")
+
+ff = RunFFTests()
+ff.testMethod()
