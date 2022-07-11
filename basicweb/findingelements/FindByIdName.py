b9cb3a27359072f53d3b71fcae0a4a537dc4be42
mrawal <imsr1013@gmail.com>
2022-07-11 12:36:02 +0545

    finding elements by id and name

---
+++ basicweb/findingelements/FindByIdName.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+
+class FindIdName():
+
+    def test(self):
+        baseUrl = "https://courses.letskodeit.com/practice"
+        driver = webdriver.Firefox()
+        driver.get(baseUrl)
+        elementById=driver.find_element_by_id("name")
+
+        if elementById is not None:
+            print("We found an element by Id")
+
+        elementByName = driver.find_element_by_name("show-hide")
+
+        if elementByName is not None:
+            print("we found an element by name")
+
+
+ff = FindIdName()
+ff.test()
