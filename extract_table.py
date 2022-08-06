eb0e0e916fbba70a84d6401a00b903a47a6647e8
mrawal <imsr1013@gmail.com>
2022-08-06 14:33:22 +0545

    extract table from website

---
+++ extract_table.py
@@ -0,0 +1,5 @@
+import pandas as pd
+simpsons=pd.read_html('https://en.wikipedia.org/wiki/List_of_The_Simpsons_episodes_(seasons_1%E2%80%9320)#Season_1_(1989%E2%80%9390)')
+print(len(simpsons))
+
+print(simpsons[1])
